8744f949d2592dc2670437329667a5f85c648435
tnosek <tnosek@student.wszib.edu.pl>
2023-10-02 16:44:24 +0200

    adding first functions for the coffee machine

---
+++ day15/main.py
@@ -0,0 +1,27 @@
+def caffe_machine():
+    menu_choice = input("What would you like? (espresso/latte/cappuccino):")
+    if menu_choice == "off":
+        off()
+    elif menu_choice == "report":
+        report()
+
+
+def off():
+    pass
+
+
+def report():
+    pass
+
+
+def if_enough(caffee):
+    pass
+
+
+def process_coins(coin_input):
+    pass
+    # return coin_rest
+
+
+def if_sufficient(coin_rest):
+    pass
