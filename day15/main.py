e8a60e80d0d44914ad1db1b5920249026b2506b8
tnosek <tnosek@student.wszib.edu.pl>
2023-10-05 19:49:20 +0200

    added functions for prcessing coins and checking ingrediens

--- day15/main.py
+++ day15/main.py
@@ -1,27 +1,42 @@
-def caffe_machine():
-    menu_choice = input("What would you like? (espresso/latte/cappuccino):")
-    if menu_choice == "off":
-        off()
-    elif menu_choice == "report":
-        report()
+from coffee_menu import MENU, resources
+
+profit = 0
+is_on = True
 
 
-def off():
-    pass
+def is_resource_sufficient(order_ingredients):
+    """Returns True when order can be made, False if ingredients are insuficient"""
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}")
+            return False
+        return True
 
 
-def report():
-    pass
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Please insert coins.")
+    total = int(input("How many quarters?: ")) * 0.25
+    total += int(input("How many dimes?: ")) * 0.1
+    total += int(input("How many nickles?: ")) * 0.05
+    total += int(input("How many pennies?: ")) * 0.01
+    return total
 
 
-def if_enough(caffee):
-    pass
+def is_transaction_successful(money_received, drink_cost):
+    """Return True when the payment is accepted, or False if money is insufficient."""
 
 
-def process_coins(coin_input):
-    pass
-    # return coin_rest
-
-
-def if_sufficient(coin_rest):
-    pass
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: {profit}")
+    else:
+        drink = MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
