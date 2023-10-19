028df76ded0ac1bb8570611039c01e27cece5f81
tnosek <tnosek@student.wszib.edu.pl>
2023-10-19 17:40:22 +0200

    changing name of the turtle

--- day18/main18-1.py
+++ day18/main18-1.py
@@ -1,9 +1,9 @@
 from turtle import Screen, Turtle
 
-timmy_the_turtle = Turtle()
+tim = Turtle()
 for _ in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+    tim.forward(100)
+    tim.right(90)
 
 screen = Screen()
 screen.exitonclick()
