f2022d8c186a62fe92170c96a54babae6ec65775
tnosek <tnosek@student.wszib.edu.pl>
2023-10-20 10:34:04 +0200

    finished challenge5 and starting finarl hirst-painting challenge

---
+++ day18/challenge5.py
@@ -0,0 +1,36 @@
+import turtle
+from turtle import Turtle, Screen
+import random as rand
+
+tim = Turtle()
+turtle.colormode(255)
+tim.speed("fastest")
+tim.pensize(2)
+
+
+def random_color():
+    r = rand.randint(0, 255)
+    g = rand.randint(0, 255)
+    b = rand.randint(0, 255)
+    random_rgb = (r, g, b)
+    return random_rgb
+
+
+def draw_circle_with_color(radius):
+    rgb = random_color()
+    tim.color(rgb)
+    tim.circle(radius)
+
+
+angle = 0
+angle_change = 7
+iterations = int(360/angle_change)
+
+for _ in range(iterations):
+    tim.setheading(angle)
+    angle += angle_change
+    draw_circle_with_color(100)
+
+
+screen = Screen()
+screen.exitonclick()
