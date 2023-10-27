000b722f94c0bcd6a9c16661f29775d7ecab0c69
tnosek <tnosek@student.wszib.edu.pl>
2023-10-27 20:26:55 +0200

    started making pong game

---
+++ day22/main.py
@@ -0,0 +1,30 @@
+from turtle import Screen, Turtle
+
+from paddle import Paddle
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.listen()
+screen.onkey(r_paddle.go_down, "Down")
+screen.update()
+screen.listen()
+screen.onkey(l_paddle.go_up, "w")
+screen.listen()
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_in_on = True
+while game_in_on:
+    screen.update()
+
+
+screen.exitonclick()
