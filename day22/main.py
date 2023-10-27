055f6c4000f87fa1e1fb729b59d6bd51959a2c49
tnosek <tnosek@student.wszib.edu.pl>
2023-10-27 21:24:18 +0200

    finished pong game

--- day22/main.py
+++ day22/main.py
@@ -3,6 +3,7 @@ from turtle import Screen, Turtle
 
 from ball import Ball
 from paddle import Paddle
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -21,12 +22,31 @@ screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
+scoreboard = Scoreboard()
 
 game_in_on = True
 while game_in_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
+    if ball.ycor() > 285 or ball.ycor() < -285:
+        ball.bounce_y()
+
+    if (
+        ball.distance(r_paddle) < 50
+        and ball.xcor() > 320
+        or ball.distance(l_paddle) < 50
+        and ball.xcor() < -320
+    ):
+        ball.bounce_x()
+
+    if ball.xcor() > 400:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -400:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
