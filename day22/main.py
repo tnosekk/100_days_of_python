0f1bb8e26da57c49f4a0668b44f8fe86f67a240a
tnosek <tnosek@student.wszib.edu.pl>
2023-10-27 20:42:20 +0200

    added ball class

--- day22/main.py
+++ day22/main.py
@@ -1,5 +1,7 @@
+import time
 from turtle import Screen, Turtle
 
+from ball import Ball
 from paddle import Paddle
 
 screen = Screen()
@@ -11,6 +13,8 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
+ball = Ball()
+
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
@@ -20,7 +24,9 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_in_on = True
 while game_in_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 
 screen.exitonclick()
