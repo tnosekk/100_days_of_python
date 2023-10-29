eac14b2327131588ba273784326fb58acbf0c079
tnosek <tnosek@student.wszib.edu.pl>
2023-10-29 16:52:32 +0100

    done player and part of car_manager in turtle crossing game

---
+++ day23/main.py
@@ -0,0 +1,33 @@
+import time
+from turtle import Screen
+
+from car_manager import CarManager
+from player import Player
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(player.move_up, "Up")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_cars()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+    if player.check_if_finish_line():
+        player.move_to_start()
+        car_manager.level_up()
