394cf2a1d5cd156cf4707e38a0b73f81f6b2ff5a
tnosek <tnosek@student.wszib.edu.pl>
2023-10-31 21:30:16 +0100

    added last funtionality to us states game

--- day25/U.S-state-quiz/main.py
+++ day25/U.S-state-quiz/main.py
@@ -20,19 +20,28 @@ def check_if_valid_state(state):
         return False
 
 
-guessed_states = 0
+guessed_states = []
 
-while guessed_states <= 50:
+while len(guessed_states) < 50:
     t = turtle.Turtle()
     t.hideturtle()
     t.penup()
     answer_state = screen.textinput(
-        title=f"{guessed_states}/50 Guess the state",
+        title=f"{len(guessed_states)}/50 States Correct",
         prompt="What's another state's name?",
     ).title()
 
     state_data = data[data["state"] == answer_state]
+    if answer_state == "Exit":
+        not_guessed = []
+        for state in states_list:
+            if state not in guessed_states:
+                not_guessed.append(state)
+
+        to_learn_df = pd.DataFrame(not_guessed)
+        to_learn_df.to_csv("day25/U.S-state-quiz/states_to_learn.csv")
+        break
     if check_if_valid_state(answer_state):
-        guessed_states += 1
+        guessed_states.append(answer_state)
         t.goto(int(state_data.x), int(state_data.y))
         t.write(arg=answer_state)
